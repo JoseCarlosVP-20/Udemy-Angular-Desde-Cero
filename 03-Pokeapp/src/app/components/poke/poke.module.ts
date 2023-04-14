78069886901d41b5621cd0983a13dc6142a2294d
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-14 03:51:49 -0600

    Grid Componente

--- 03-Pokeapp/src/app/components/poke/poke.module.ts
+++ 03-Pokeapp/src/app/components/poke/poke.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { CardComponent } from './card/card.component';
 import { GridComponent } from './grid/grid.component';
@@ -9,7 +10,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [GridComponent, NavbarComponent, CardComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatGridListModule],
   exports: [GridComponent],
 })
 export class PokeModule {}
