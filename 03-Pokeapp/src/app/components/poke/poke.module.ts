207abf26f8e80de5f7d4821f7507ab96c21a3376
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-14 16:13:14 -0600

    Card Componente

--- 03-Pokeapp/src/app/components/poke/poke.module.ts
+++ 03-Pokeapp/src/app/components/poke/poke.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CardComponent } from './card/card.component';
 import { GridComponent } from './grid/grid.component';
@@ -10,7 +12,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [GridComponent, NavbarComponent, CardComponent],
-  imports: [CommonModule, MatToolbarModule, MatGridListModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    HttpClientModule,
+  ],
   exports: [GridComponent],
 })
 export class PokeModule {}
