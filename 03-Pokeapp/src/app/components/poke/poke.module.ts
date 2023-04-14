c8768397690f9f6dfab8be9a7c8d28293b5071ac
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-14 03:40:51 -0600

    Header Componente

--- 03-Pokeapp/src/app/components/poke/poke.module.ts
+++ 03-Pokeapp/src/app/components/poke/poke.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { CardComponent } from './card/card.component';
 import { GridComponent } from './grid/grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [GridComponent, NavbarComponent, CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   exports: [GridComponent],
 })
 export class PokeModule {}
