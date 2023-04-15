d35542491b227120ae8f22904ad9346b4aa3bbb9
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-14 19:11:20 -0600

    HTTP Client

--- 03-Pokeapp/src/app/components/poke/grid/grid.component.ts
+++ 03-Pokeapp/src/app/components/poke/grid/grid.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PokeService } from 'src/app/services/poke.service';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css'],
 })
 export class GridComponent {
-
+  constructor(private pokeService: PokeService) {
+    pokeService.getList().subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
