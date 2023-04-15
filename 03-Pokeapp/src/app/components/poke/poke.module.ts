07d6ac480ac4e8243b25934a33264a949b631fa2
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-15 01:37:20 -0600

    Material Icons

--- 03-Pokeapp/src/app/components/poke/poke.module.ts
+++ 03-Pokeapp/src/app/components/poke/poke.module.ts
@@ -2,9 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 import { CardComponent } from './card/card.component';
 import { GridComponent } from './grid/grid.component';
@@ -18,6 +21,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     MatGridListModule,
     MatCardModule,
     HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [GridComponent],
 })
