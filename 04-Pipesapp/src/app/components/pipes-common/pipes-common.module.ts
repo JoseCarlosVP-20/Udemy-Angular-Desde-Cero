b2a3b827b87948379314d63ae8fbc9ff10a216c1
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-21 00:42:36 -0600

    Pipe Slice

--- 04-Pipesapp/src/app/components/pipes-common/pipes-common.module.ts
+++ 04-Pipesapp/src/app/components/pipes-common/pipes-common.module.ts
@@ -6,9 +6,10 @@ import { NumeroComponent } from './numero/numero.component';
 import { FechaComponent } from './fecha/fecha.component';
 import { JsonComponent } from './json/json.component';
 import { KeyValueComponent } from './key-value/key-value.component';
+import { SliceComponent } from './slice/slice.component';
 
 @NgModule({
-  declarations: [ContainerComponent, TextoComponent, NumeroComponent, FechaComponent, JsonComponent, KeyValueComponent],
+  declarations: [ContainerComponent, TextoComponent, NumeroComponent, FechaComponent, JsonComponent, KeyValueComponent, SliceComponent],
   imports: [CommonModule],
   exports: [ContainerComponent],
 })
