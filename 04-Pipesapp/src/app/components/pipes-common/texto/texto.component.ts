73bf9453210b36d3f7983d360eb79014a00f156b
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-20 21:25:32 -0600

    Bootstrap y pipes lowercase uppercase y titlecase

--- 04-Pipesapp/src/app/components/pipes-common/texto/texto.component.ts
+++ 04-Pipesapp/src/app/components/pipes-common/texto/texto.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-texto',
   templateUrl: './texto.component.html',
-  styleUrls: ['./texto.component.css']
+  styleUrls: ['./texto.component.css'],
 })
 export class TextoComponent {
-
+  texto: string = 'TiTuLO de AplIcaCIoN';
 }
