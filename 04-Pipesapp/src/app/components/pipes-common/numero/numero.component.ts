139f22d62dbf4772c3c096414593cf13dfb70fa5
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-20 23:04:50 -0600

    Decimal, Percent, Currency

---
+++ 04-Pipesapp/src/app/components/pipes-common/numero/numero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numero',
+  templateUrl: './numero.component.html',
+  styleUrls: ['./numero.component.css'],
+})
+export class NumeroComponent {
+  calificacion: number = 7.916974619961;
+  porcentaje: number = 0.36;
+  saldo: number = 12000;
+}
--- 04-Pipesapp/src/app/components/pipes-common/pipes-common.module.ts
+++ 04-Pipesapp/src/app/components/pipes-common/pipes-common.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContainerComponent } from './container/container.component';
 import { TextoComponent } from './texto/texto.component';
+import { NumeroComponent } from './numero/numero.component';
 
 @NgModule({
-  declarations: [ContainerComponent, TextoComponent],
+  declarations: [ContainerComponent, TextoComponent, NumeroComponent],
   imports: [CommonModule],
   exports: [ContainerComponent],
 })
