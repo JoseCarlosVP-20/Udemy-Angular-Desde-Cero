5b97e5f7ed379410ed0a6f0fb34894ae08e1e68d
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-22 16:01:41 -0600

    Subscribe

--- 05-RxJS-directivas/src/app/components/reactive/container/container.component.ts
+++ 05-RxJS-directivas/src/app/components/reactive/container/container.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  styleUrls: ['./container.component.css'],
 })
 export class ContainerComponent {
+  constructor() {
+    let numero = 0;
 
+    const miObservable = new Observable<number>((observer) => {
+      setInterval(() => {
+        numero++;
+        observer.next(numero)
+      }, 1000);
+    });
+
+    miObservable.subscribe((result)=>{
+      console.log(result);
+    });
+  }
 }
