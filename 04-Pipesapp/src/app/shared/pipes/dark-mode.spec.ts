852e0359bd3a8a2f2c8bb47b14138abbebaa7434
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-22 03:08:27 -0600

    Prueba a Pipe simple

--- 04-Pipesapp/src/app/shared/pipes/dark-mode.spec.ts
+++ 04-Pipesapp/src/app/shared/pipes/dark-mode.spec.ts
@@ -3,5 +3,12 @@ import { DarkModePipe } from './dark-mode.pipe';
 describe('DarModePipe', () => {
   it('Debería retornar DarkMode si mando true', () => {
     const pipe = new DarkModePipe();
+
+    expect(pipe.transform(true)).toEqual('bg-dark text-white');
+  });
+  it('Debería retornar LightMode si mando false', () => {
+    const pipe = new DarkModePipe();
+
+    expect(pipe.transform(false)).toEqual('bg-light text-dark');
   });
 });
