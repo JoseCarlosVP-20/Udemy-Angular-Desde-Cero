1d1a1c541b10c4397d19aab32226f13c7de33930
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-22 04:36:29 -0600

    Lifecycle Hooks

--- 05-RxJS-directivas/src/app/app.component.ts
+++ 05-RxJS-directivas/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs-directivas';
+
+  ngOnInit(): void {
+    this.title = 'Componente eliminado';
+    console.log(this.title);
+  }
+
+  ngOnDestroy(): void {
+    this.title = 'Componente creado';
+    console.log(this.title);
+  }
 }
