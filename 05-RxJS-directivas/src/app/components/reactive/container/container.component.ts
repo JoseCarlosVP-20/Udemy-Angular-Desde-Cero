0de04c56dd6b2d98e64a73213f9e82a51114d2ee
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-22 22:35:03 -0600

    Interval RxJS

--- 05-RxJS-directivas/src/app/components/reactive/container/container.component.ts
+++ 05-RxJS-directivas/src/app/components/reactive/container/container.component.ts
@@ -1,49 +1,17 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css'],
 })
-export class ContainerComponent {
-  constructor() {
-    let numero = 0;
+export class ContainerComponent implements OnInit {
+  miIntervalo: Observable<number> = interval(3000);
 
-    const miObservable = new Observable<number>((observer) => {
-      setInterval(() => {
-        numero++;
-        observer.next(numero);
+  constructor() {}
 
-        observer.complete();
-
-        if (numero == 3) observer.error('Número erroneo');
-      }, 1000);
-    });
-
-    miObservable.subscribe(
-      // LEGACY
-      // (result) => {
-      //   console.log(result);
-      // },
-      // (error) => {
-      //   console.error(error);
-      // },
-      // () => {
-      //   console.log('Observable Completado');
-      // }
-
-      {
-        next: (result) => {
-          console.log(result);
-        },
-        error: (error) => {
-          console.error(error);
-        },
-        complete: () => {
-          console.log('Completado');
-        },
-      }
-    );
+  ngOnInit(): void {
+    this.miIntervalo.subscribe((value) => console.log(value));
   }
 }
