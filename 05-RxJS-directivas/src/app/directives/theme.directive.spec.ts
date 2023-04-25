d2ecd640ab19aefb0e678bff4a33d81b99b59211
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-25 00:41:36 -0600

    Configuración de la prueba

--- 05-RxJS-directivas/src/app/directives/theme.directive.spec.ts
+++ 05-RxJS-directivas/src/app/directives/theme.directive.spec.ts
@@ -1,8 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from '../app.component';
+import { ContainerComponent } from '../components/reactive/container/container.component';
 import { ThemeDirective } from './theme.directive';
 
 describe('ThemeDirective', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    fixture = await TestBed.configureTestingModule({
+      declarations: [AppComponent, ThemeDirective, ContainerComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new ThemeDirective();
-    expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
