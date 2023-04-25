670a9a67ef515f78c32e555112aa05aac56b8540
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-25 02:34:18 -0600

    Prueba AreaMouseDirective

--- 05-RxJS-directivas/src/app/components/reactive/directives/area-mouse.directive.spec.ts
+++ 05-RxJS-directivas/src/app/components/reactive/directives/area-mouse.directive.spec.ts
@@ -1,8 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContainerComponent } from '../container/container.component';
 import { AreaMouseDirective } from './area-mouse.directive';
 
 describe('AreaMouseDirective', () => {
+  let component: ContainerComponent;
+  let fixture: ComponentFixture<ContainerComponent>;
+  let divElement: HTMLDivElement;
+
+  beforeEach(async () => {
+    fixture = await TestBed.configureTestingModule({
+      declarations: [ContainerComponent, AreaMouseDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    // const directive = new AreaMouseDirective();
-    // expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('Debería crear un div con un color de fondo BLUE, tamaño del 300px', () => {
+    divElement = fixture.debugElement.nativeElement.querySelector('div');
+    expect(divElement.style.backgroundColor).toBe('blue');
+    expect(divElement.style.height).toBe('300px');
   });
 });
