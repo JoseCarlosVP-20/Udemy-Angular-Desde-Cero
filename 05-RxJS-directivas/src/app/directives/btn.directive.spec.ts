cd40b97362ede402260f775d4ebdc533ae55a046
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-25 02:21:18 -0600

    Prueba BtnDirective

--- 05-RxJS-directivas/src/app/directives/btn.directive.spec.ts
+++ 05-RxJS-directivas/src/app/directives/btn.directive.spec.ts
@@ -1,8 +1,44 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+import { ContainerComponent } from '../components/reactive/container/container.component';
 import { BtnDirective } from './btn.directive';
 
 describe('BtnDirective', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let buttonElement: HTMLButtonElement;
+
+  beforeEach(async () => {
+    fixture = await TestBed.configureTestingModule({
+      declarations: [AppComponent, BtnDirective, ContainerComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    buttonElement = fixture.debugElement.query(
+      By.directive(BtnDirective)
+    ).nativeElement;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    // const directive = new BtnDirective();
-    // expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('Debería crear un botón con un color RED con fondo RED, color del texto SNOW, tamaño del 100%', () => {
+    expect(buttonElement.style.backgroundColor).toBe('purple');
+    expect(buttonElement.style.color).toBe('snow');
+    expect(buttonElement.style.width).toBe('100%');
+  });
+
+  it('Debería tener el WIDTH en 100%', () => {
+    expect(buttonElement.style.width).toBe('100%');
+  });
+
+  it('Debería tener el PADDING en .3rem', () => {
+    expect(buttonElement.style.padding).toBe('0.3rem');
   });
 });
--- 05-RxJS-directivas/src/app/directives/theme.directive.spec.ts
+++ 05-RxJS-directivas/src/app/directives/theme.directive.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from '../app.component';
 import { ContainerComponent } from '../components/reactive/container/container.component';
 import { ThemeDirective } from './theme.directive';
@@ -21,5 +22,14 @@ describe('ThemeDirective', () => {
 
   it('should create an instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debe crear un DIV con el backgroundColor #333 y el color del texto Snow', () => {
+    const element: HTMLDivElement = fixture.debugElement.query(
+      By.directive(ThemeDirective)
+    ).nativeElement;
+
+    expect(element.style.backgroundColor).toBe('rgb(51, 51, 51)');
+    expect(element.style.color).toBe('snow');
   });
 });
