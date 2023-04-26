a87c36d3a05adb8ef30a0bc8e5d333e764b18150
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-04-26 02:04:36 -0600

    Capturando datos de Input

--- 06-Formularios/src/app/components/formulario-template/formulario-template/formulario-template.component.ts
+++ 06-Formularios/src/app/components/formulario-template/formulario-template/formulario-template.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-template',
   templateUrl: './formulario-template.component.html',
-  styleUrls: ['./formulario-template.component.css']
+  styleUrls: ['./formulario-template.component.css'],
 })
 export class FormularioTemplateComponent {
+  @ViewChild('miFormulario')
+  miFormulario!: NgForm;
 
+  agregar() {
+    console.log(this.miFormulario.value);
+  }
 }
