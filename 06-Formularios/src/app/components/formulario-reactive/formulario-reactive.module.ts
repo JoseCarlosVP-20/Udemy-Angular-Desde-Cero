6fa6c29de101412c75671bb112273f1fcfef2197
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-05-01 02:27:16 -0600

    FormGroup, FormBuilder, Validators

--- 06-Formularios/src/app/components/formulario-reactive/formulario-reactive.module.ts
+++ 06-Formularios/src/app/components/formulario-reactive/formulario-reactive.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormularioReactiveComponent } from './formulario-reactive/formulario-reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormularioReactiveComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [FormularioReactiveComponent],
 })
 export class FormularioReactiveModule {}
--- 06-Formularios/src/app/components/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
+++ 06-Formularios/src/app/components/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactive',
   templateUrl: './formulario-reactive.component.html',
-  styleUrls: ['./formulario-reactive.component.css']
+  styleUrls: ['./formulario-reactive.component.css'],
 })
 export class FormularioReactiveComponent {
+  miFormulario: FormGroup = this.fb.group({
+    proyectoNombre: this.fb.control('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(15),
+    ]),
+    horas: this.fb.control(0, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(50),
+    ]),
+    tecnologias: this.fb.control(''),
+  });
 
+  constructor(private fb: FormBuilder) {}
 }
