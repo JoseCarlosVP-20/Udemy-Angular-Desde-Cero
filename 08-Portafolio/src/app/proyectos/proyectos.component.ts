7516a36b8c1456fd3c7ef35cd0f01204a2a620a8
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-05-04 19:41:57 -0600

    Servicios

--- 08-Portafolio/src/app/proyectos/proyectos.component.ts
+++ 08-Portafolio/src/app/proyectos/proyectos.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ProyectosService } from './service/proyectos.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
-  styleUrls: ['./proyectos.component.css']
+  styleUrls: ['./proyectos.component.css'],
 })
 export class ProyectosComponent {
-
+  constructor(private proyectosServices: ProyectosService) {}
+  get data() {
+    return this.proyectosServices.data;
+  }
 }
