da529d2fb7ec68dd12ab52350d202841f09d878e
Jose Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2023-05-05 03:30:22 -0600

    ActiveRouter

--- 08-Portafolio/src/app/proyectos/informacion/informacion.component.ts
+++ 08-Portafolio/src/app/proyectos/informacion/informacion.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
-  styleUrls: ['./informacion.component.css']
+  styleUrls: ['./informacion.component.css'],
 })
 export class InformacionComponent {
-
+  parametro: string = '';
+  constructor(private router: ActivatedRoute) {
+    let nombre = '';
+    router.params.subscribe(({ nombre: nombreProyecto }) => {
+      nombre = nombreProyecto;
+    });
+    this.parametro = decodeURI(nombre);
+  }
 }
